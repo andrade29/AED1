6a9dc5d8e24caf817ea362ba30bc175a3bac3a7c
Caio Andrade <163180958+andrade29@users.noreply.github.com>
2026-01-07 20:37:24 -0300

    ajustes nos comentários do código

--- 1610-lista-adj.c
+++ 1610-lista-adj.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// struct de uma lista adj
 typedef struct {
-    int *adj;
-    int qtd, cap;
+    int *adj;   // vetor de adjacentes
+    int qtd, cap; 
 } Lista;
 
+// adiciona aresta na lista
 void addAresta(Lista *L, int x) {
     if (L->qtd == L->cap) {
         L->cap = (L->cap == 0 ? 2 : L->cap * 2);
@@ -14,11 +16,12 @@ void addAresta(Lista *L, int x) {
     L->adj[L->qtd++] = x;
 }
 
-int acheiCiclo;
+int acheiCiclo;  
 int *cor; // 0 = não visitei, 1 = visitando, 2 = terminei
 
+// funcao DFS para detectar ciclo em grafo direcionado
 void dfs(Lista *g, int atual) {
-    cor[atual] = 1;
+    cor[atual] = 1; // marca como visitando
 
     for (int i = 0; i < g[atual].qtd; i++) {
         int prox = g[atual].adj[i];
@@ -28,47 +31,48 @@ void dfs(Lista *g, int atual) {
             if (acheiCiclo) return;
         }
         else if (cor[prox] == 1) {
+            // encontrou um ciclo
             acheiCiclo = 1;
             return;
         }
     }
-    cor[atual] = 2;
+
+    cor[atual] = 2; // marca como finalizado
 }
 
 int main() {
-    int T;
+    int T;  // t representa número de casos de teste
     scanf("%d", &T);
 
     while (T--) {
-        int N, M;
+        int N, M;  // n vértices e m arestas
         scanf("%d %d", &N, &M);
 
+        // aloca o grafo e o vetor de cores e depois faz leitura das arestas
         Lista *g = (Lista*)calloc(N + 1, sizeof(Lista));
         cor = (int*)calloc(N + 1, sizeof(int));
-
         for (int i = 0; i < M; i++) {
             int A, B;
             scanf("%d %d", &A, &B);
             addAresta(&g[A], B);
         }
-
         acheiCiclo = 0;
-
+        // executa DFS para cada vértice não visitado
         for (int i = 1; i <= N && !acheiCiclo; i++) {
             if (cor[i] == 0) {
                 dfs(g, i);
             }
         }
-
         if (acheiCiclo)
             printf("SIM\n");
         else
             printf("NAO\n");
 
+        //libera
         for (int i = 1; i <= N; i++) free(g[i].adj);
         free(g);
         free(cor);
     }
 
-    return 0;
+    return 0;  
 }
