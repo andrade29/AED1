78461ba3cd687b984874738a74bac314981a8bf9
Caio Andrade <163180958+andrade29@users.noreply.github.com>
2026-01-07 20:05:21 -0300

    ajustes nos comentários do código

--- 1566_SelectionSort.c
+++ 1566_SelectionSort.c
@@ -1,48 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// funcao selectionSort
 void selectionSort(int array[], int n) {
     for(int i = 0; i < n-1; i++) {
-        int min_idx = i; // índice do menor elemento na sublista restante
+        int min_idx = i; // menor elemento na lista
+
+        // for para procurar o menor elemento
         for(int j = i+1; j < n; j++) {
             if(array[j] < array[min_idx]) {
                 min_idx = j;
             }
         }
-        // troca o menor encontrado com o primeiro elemento da sublista
+
+        // faz a troca do menor encontrado com o atual
         int temp = array[i];
         array[i] = array[min_idx];
         array[min_idx] = temp;
     }
 }
 
-
 int main(){
-    int casos;
+    int casos;                
     scanf("%d", &casos);
 
     while(casos--){
-        int pessoas;
+        int pessoas;          
         scanf("%d", &pessoas);
 
+        // aloca o vetor e faz a leitura dos valores
         int *array_pessoas = (int*) malloc(pessoas * sizeof(int));
-        
-        for(int i =0; i < pessoas; i++){
+        for(int i = 0; i < pessoas; i++){
             scanf("%d", &array_pessoas[i]);
         }
-
+        // ordena usando selection sort
         selectionSort(array_pessoas, pessoas);
 
-        for(int i=0; i < pessoas; i++){
-            printf("%d ", array_pessoas[i]);
+        // imprime o vetor ordenado
+        for(int i = 0; i < pessoas; i++){
+            printf("%d", array_pessoas[i]);
             if(i < pessoas - 1) printf(" ");
         }
         printf("\n");
-
-        free(array_pessoas);
+        free(array_pessoas);  
     }
-
-
-
-
 }
