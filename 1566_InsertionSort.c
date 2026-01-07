4d933a1b5fbdd34cc2cd7f6d3c0c95ec16d6d8e1
Caio Andrade <163180958+andrade29@users.noreply.github.com>
2026-01-07 19:53:47 -0300

    ajustes nos comentários do código

--- 1566_InsertionSort.c
+++ 1566_InsertionSort.c
@@ -1,46 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// funcao insertionSort
 void insertion(int array[], int n){
     for(int i = 1; i < n; i++){
-        int chave = array[i];
+        int chave = array[i];  
         int j = i - 1;
 
-    while(j >= 0 && array[j] > chave){
-        array[j+1] = array[j];
-        j--;
+        // elementos maiores a direita
+        while(j >= 0 && array[j] > chave){
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1] = chave;
     }
-    array[j+1] = chave;
 }
-}
-
 
 int main(){
-    int casos;
+    int casos; // casos testes
     scanf("%d", &casos);
 
     while(casos--){
         int pessoas;
         scanf("%d", &pessoas);
 
+        // aloca o vetor e depois faz a leitura dos valores
         int *array_pessoas = (int*) malloc(pessoas * sizeof(int));
-        
-        for(int i =0; i < pessoas; i++){
+        for(int i = 0; i < pessoas; i++){
             scanf("%d", &array_pessoas[i]);
         }
 
+        // ordena
         insertion(array_pessoas, pessoas);
 
-        for(int i=0; i < pessoas; i++){
-            printf("%d ", array_pessoas[i]);
+        // for para mostrar os valores ordenados
+        for(int i = 0; i < pessoas; i++){
+            printf("%d", array_pessoas[i]);
             if(i < pessoas - 1) printf(" ");
         }
         printf("\n");
-
-        free(array_pessoas);
+        free(array_pessoas); 
     }
-
-
-
-
 }
