1f7bc49cef0d4c6afa1dc485884cc1760404b85d
Caio Andrade <163180958+andrade29@users.noreply.github.com>
2026-01-07 20:02:40 -0300

    ajustes nos comentários do código

--- 1566_QuickSort.c
+++ 1566_QuickSort.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int partition(int v[], int inicio, int fim) {
-    int pivo = v[fim];
-    int i = inicio - 1;
+// funcao particao -> aonde usa o ultimo elemento como pivo
+int particao(int v[], int inicio, int fim) {
+    int pivo = v[fim]; 
+    int i = inicio - 1; // índice do menor elemento
 
+    // percorre o vetor e reorganiza em relação ao pivo
     for (int j = inicio; j < fim; j++) {
         if (v[j] <= pivo) {
             i++;
@@ -14,47 +16,50 @@ int partition(int v[], int inicio, int fim) {
         }
     }
 
+    // ajusta o pivo no lugar certo
     int aux = v[i+1];
     v[i+1] = v[fim];
     v[fim] = aux;
-
-    return i + 1; 
+    return i + 1;     
 }
 
+// funcao quickSorte
 void quicksort(int v[], int inicio, int fim) {
     if (inicio < fim) {
-        int p = partition(v, inicio, fim);
+        int p = particao(v, inicio, fim);
         quicksort(v, inicio, p - 1);
         quicksort(v, p + 1, fim);
     }
 }
 
 int main() {
-    int casos;
+    int casos;                  
     scanf("%d", &casos);
 
     while (casos--) {
-        int M;
+        int M;    // m representa a quantidade de elementos
         scanf("%d", &M);
 
-        int *original =(int*) malloc(M * sizeof(int));
-        int *ordenado =(int*) malloc(M * sizeof(int));
+        // vetor original e vetor ordenado
+        int *original = (int*) malloc(M * sizeof(int));
+        int *ordenado = (int*) malloc(M * sizeof(int));
 
+        // faz a leitura no vetor original e depois passa para o vetor ordenado
         for (int i = 0; i < M; i++)
             scanf("%d", &original[i]);
-
         for (int i = 0; i < M; i++)
             ordenado[i] = original[i];
 
+        // ordena o vetor copiado
         quicksort(ordenado, 0, M - 1);
+        int cont = 0;           
 
-        int cont = 0;
+        // for para comparar os dois vetores
         for (int i = 0; i < M; i++)
             if (ordenado[i] == original[i])
                 cont++;
 
         printf("%d\n", cont);
-
         free(original);
         free(ordenado);
     }
