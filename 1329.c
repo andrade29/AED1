5f6d4e002c9b7de3d7ac7a0fc9975bb5e9f13d93
Caio Andrade <163180958+andrade29@users.noreply.github.com>
2026-01-07 16:32:50 -0300

    ajustes nos comentários do código

--- 1329.c
+++ 1329.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int N;
+    int N;  // n representa o numero de partidas
     
+    // loop que roda enquanto e difrente de zero
     while(scanf("%d", &N) && N != 0) {
-        int mary = 0, john = 0;
+        int mary = 0, john = 0; 
+        
+        // for para os resultados da partida
         for(int i = 0; i < N; i++) {
             int resultado;
             scanf("%d", &resultado);
-            if(resultado == 0){
+            
+            if(resultado == 0){ //-> mary ganhou
                 mary++;
             }
-            else if(resultado == 1){
+            else if(resultado == 1){ //-> john ganhou
                 john++;
             }
         }
