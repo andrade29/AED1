70cfd942e646c72117bb1f85385184e3a1891791
Caio Andrade <163180958+andrade29@users.noreply.github.com>
2026-01-07 16:22:43 -0300

    ajustes nos comentários do código

--- 1257.c
+++ 1257.c
@@ -2,29 +2,31 @@
 #include <string.h>
 
 int main() {
-    int N; // número de casos de teste
+    int N; // n representa o numero de casos testes
     scanf("%d", &N);
 
-    for(int caso=0; caso<N; caso++) {
-        int L; // número de linhas do caso
+    // for para cada caso teste
+    for(int caso = 0; caso < N; caso++) {
+        int L;
         scanf("%d", &L);
-        getchar(); 
+        getchar();
+        int hash = 0; 
 
-        int hash = 0;
-
-        for(int i=0; i<L; i++) {
+        // for para ler as
+        for(int i = 0; i < L; i++) {
             char linha[55];
             fgets(linha, sizeof(linha), stdin);
+
+            // remove os quebras de linhas
             linha[strcspn(linha, "\n")] = 0;
 
-            for(int j=0; linha[j]; j++) {
-                int posAlfabeto = linha[j] - 'A';
-                hash += posAlfabeto + i + j;
+            for(int j = 0; linha[j]; j++) {
+                int posAlfabeto = linha[j] - 'A'; 
+                hash += posAlfabeto + i + j;    
             }
         }
-
         printf("%d\n", hash);
     }
 
-    return 0;
+    return 0; 
 }
